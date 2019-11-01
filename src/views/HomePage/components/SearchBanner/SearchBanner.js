25dc60a3f1af5808131de682edb1260f863e44d5
Miriam <miriam.aparicio@gmail.com>
2019-11-01 18:14:44 +0100

    Implement final design to home page

--- src/views/HomePage/components/SearchBanner/SearchBanner.js
+++ src/views/HomePage/components/SearchBanner/SearchBanner.js
@@ -7,12 +7,22 @@ import SearchInput from '../SearchInput/SearchInput';
 class SearchBanner extends Component {
   render() {
     return (
-      <section className="section search-banner">
-        <div className="container has-text-centered">
-          <h2 className="subtitle search-banner-subtitle">
-            Cerca serveis a la teva zona
-          </h2>
-          <SearchInput></SearchInput>
+      <section className="hero is-small search-banner">
+        <div className="hero-body">
+          <div className="container columns is-vcentered is-centered">
+            <div className="column has-text-centered search">
+              <img
+                src={require('../../../../images/cerca.png')}
+                alt="imatge-cerca"
+              />
+            </div>
+            <div className="column has-text-centered">
+              <h2 className="title is-2 search-banner-subtitle">
+                Cerca serveis a la teva zona
+              </h2>
+              <SearchInput></SearchInput>
+            </div>
+          </div>
         </div>
       </section>
     );
